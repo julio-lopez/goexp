9418eefd942b70af6ffd19ec29546a4d09be9d02
Julio Lopez <julio@kasten.io>
2021-09-10 22:31:37 -0700

    update clone-kopia.sh

--- docker/golang-idle/clone-kopia.sh
+++ docker/golang-idle/clone-kopia.sh
@@ -4,6 +4,14 @@ set -o errexit
 set -o nounset
 set +o xtrace
 
-pushd /mnt
+readonly SRC_DIR=${PATH_PREFIX:-/mnt}/src
+
+mkdir -p ${SRC_DIR}
+
+pushd ${SRC_DIR}
 
 git clone https://github.com/kastenhq/kopia
+
+cd kopia
+
+make build-current-os-noui
