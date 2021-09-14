92b3447538d49ecb4e58fc6e6ce353c184ea4e6a
Julio Lopez <julio@kasten.io>
2021-09-13 17:54:23 -0700

    add tini to golang-idle image

--- docker/golang-idle/build.sh
+++ docker/golang-idle/build.sh
@@ -5,13 +5,16 @@ set -o nounset
 set -o xtrace
 #set -o pipefail
 
+readonly buildVersion="$(git log -1 --format=%ad-%h --date=format:'%Y.%d.%m-%H%m')"
 readonly ts="$(date +%Y.%m.%d-%H%M%S)"
 readonly basedir=$(realpath --logical --canonicalize-existing "$(dirname ${0})")
 
 cd "${basedir}"
 
 docker build \
-    --build-arg imageVersion="${ts}" \
-    --tag "ghcr.io/julio-lopez/golang:idle-${ts}" .
+    --build-arg imageVersion="${buildVersion}" \
+    --label buildVersion="${buildVersion}" \
+    --label buildDate="${ts}" \
+    --tag "ghcr.io/julio-lopez/golang-idle:${buildVersion}" .
 
 cd -
