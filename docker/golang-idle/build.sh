af289ae3f67cd7643e2f3e7e253724a85cd26def
Julio Lopez <1953782+julio-lopez@users.noreply.github.com>
2022-04-14 22:33:39 -0700

    fix date in image tag

--- docker/golang-idle/build.sh
+++ docker/golang-idle/build.sh
@@ -5,7 +5,7 @@ set -o nounset
 set -o xtrace
 #set -o pipefail
 
-readonly buildVersion="$(git log -1 --format=%ad-%h --date=format:'%Y.%d.%m-%H%m')"
+readonly buildVersion="$(git log -1 --format=%ad-%h --date=format:'%Y.%m.%d-%H%m')"
 readonly ts="$(date +%Y.%m.%d-%H%M%S)"
 readonly basedir=$(realpath --logical --canonicalize-existing "$(dirname ${0})")
 
