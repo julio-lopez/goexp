b8ece0c94f826b08283bd3761e295dc1bee8ae17
Julio Lopez <1953782+julio-lopez@users.noreply.github.com>
2022-06-01 16:04:25 -0700

    add 'latest' tag

--- docker/go-idle/build.sh
+++ docker/go-idle/build.sh
@@ -5,17 +5,19 @@ set -o nounset
 set -o xtrace
 #set -o pipefail
 
+readonly IMAGE="ghcr.io/julio-lopez/go-idle"
 readonly buildVersion="$(git log -1 --format=%ad-%h --date=format:'%Y.%m.%d-%H%m')"
 readonly ts="$(date +%Y-%m-%d-%H%M%S)"
 readonly basedir=$(realpath --logical --canonicalize-existing "$(dirname ${0})")
 
 cd "${basedir}"
 
-#    --label buildDate="${ts}" \
 
 docker build \
     --build-arg imageVersion="${buildVersion}" \
-    --tag "ghcr.io/julio-lopez/go-idle:${buildVersion}" \
-    --tag "ghcr.io/julio-lopez/go-idle:${buildVersion}-${ts}" .
+    --tag "${IMAGE}:${buildVersion}" \
+    --tag "${IMAGE}:${buildVersion}-${ts}" \
+    --tag "${IMAGE}:latest" \
+    .
 
 cd -
