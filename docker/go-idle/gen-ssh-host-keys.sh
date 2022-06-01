06ca6d13d38ed18af4657ac6ffe036f73201e176
Julio Lopez <1953782+julio-lopez@users.noreply.github.com>
2022-06-01 11:07:19 -0700

    script to generate SSH host keys

---
+++ docker/go-idle/gen-ssh-host-keys.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+
+set -o errexit
+set -o nounset
+set -o xtrace
+
+
+create_key() {
+    local -r key_file="$1"
+    shift
+
+    echo "Generating SSH key and saving it as '${key_file}'"
+    ssh-keygen -q -f "${key_file}" -C "host key" -N '' "$@"
+    ssh-keygen -l -f "${key_file}.pub"
+}
+
+readonly KEYS_DIR=${1:-"keys"}
+
+mkdir --parents --mode=700 "${KEYS_DIR}"
+
+create_key "${KEYS_DIR}/ssh_host_ed25519_key" -t ed25519
+create_key "${KEYS_DIR}/ssh_host_dsa_key" -t dsa
+create_key "${KEYS_DIR}/ssh_host_rsa_key" -t rsa
+
+# Alternatively:
+#
+# mkdir --parents --mode=700 "${KEYS_DIR}/etc/ssh"
+# ssh-keygen -A -f "${KEYS_DIR}"
