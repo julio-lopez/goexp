845ea0057bc5e6f52e2d139794eca6833c201a78
Julio Lopez <1953782+julio-lopez@users.noreply.github.com>
2022-06-01 18:07:32 -0700

    include seconds in git version when building the image

--- docker/go-idle/build.sh
+++ docker/go-idle/build.sh
@@ -6,17 +6,17 @@ set -o xtrace
 #set -o pipefail
 
 readonly IMAGE="ghcr.io/julio-lopez/go-idle"
-readonly buildVersion="$(git log -1 --format=%ad-%h --date=format:'%Y.%m.%d-%H%m')"
+readonly buildVersion="$(git log -1 --format=%ad-%h --date=format:'%Y.%m.%d-%H%m%S')"
 readonly ts="$(date +%Y-%m-%d-%H%M%S)"
 readonly basedir=$(realpath --logical --canonicalize-existing "$(dirname ${0})")
 
 cd "${basedir}"
 
+#     --tag "${IMAGE}:${buildVersion}-${ts}" \
 
 docker build \
     --build-arg imageVersion="${buildVersion}" \
     --tag "${IMAGE}:${buildVersion}" \
-    --tag "${IMAGE}:${buildVersion}-${ts}" \
     --tag "${IMAGE}:latest" \
     .
 
