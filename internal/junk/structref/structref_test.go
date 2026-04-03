3d78e8ecb44da33fbe8de69d896b5b16befb13dd
J <1953782+julio-lopez@users.noreply.github.com>
2026-04-02 22:04:33 -0700

    fix linter annotations

--- internal/junk/structref/structref_test.go
+++ internal/junk/structref/structref_test.go
@@ -49,7 +49,7 @@ type (
 func (r refArray4) sliceAll() []nodeRef  { return r[:] }
 func (r refArray16) sliceAll() []nodeRef { return r[:] }
 
-//lint:ignore U1000
+//lint:ignore U1000 WIP
 //nolint:unused
 type slicerAll interface {
 	sliceAll() []nodeRef
@@ -94,20 +94,20 @@ func (n *node16) getChildrenCapacity() int {
 	return node16Max
 }
 
-//lint:ignore U1000
+//lint:ignore U1000 WIP
 //nolint:unused
 type refArray interface {
 	refArray4 | refArray16
 }
 
-//lint:ignore U1000
+//lint:ignore U1000 WIP
 //nolint:unused
 type node[R slicerAll] struct {
 	refs R
 	nodeBase
 }
 
-//lint:ignore U1000
+//lint:ignore U1000 WIP
 //nolint:unused
 func (n *node[R]) getChildren() []nodeRef {
 	return n.refs.sliceAll()[:n.childCount]
