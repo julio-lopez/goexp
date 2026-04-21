9eecf5273638437f5f4fcecf04fa6b3530f8daed
J <1953782+julio-lopez@users.noreply.github.com>
2026-04-21 11:04:13 -0700

    go-idle image

---
+++ docker/go-idle/build.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+set -o errexit
+set -o nounset
+set -o xtrace
+#set -o pipefail
+
+readonly IMAGE="ghcr.io/julio-lopez/go-idle"
+readonly buildVersion="$(git log -1 --format=%ad-%h --date=format:'%Y.%m.%d-%H%m%S')"
+readonly ts="$(date +%Y-%m-%d-%H%M%S)"
+readonly basedir=$(realpath --logical --canonicalize-existing "$(dirname ${0})")
+
+cd "${basedir}"
+
+#     --tag "${IMAGE}:${buildVersion}-${ts}" \
+
+docker build \
+    --build-arg imageVersion="${buildVersion}" \
+    --tag "${IMAGE}:${buildVersion}" \
+    --tag "${IMAGE}:latest" \
+    .
+
+cd -
